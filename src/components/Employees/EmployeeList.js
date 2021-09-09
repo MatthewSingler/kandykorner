3e8582efda3bb14a4cbceb6df356eea4de4c74fe
Matthew Singler <matthew.singler@gmail.com>
2021-09-09 15:58:08 -0500

    Created some basic framework for employee list and employee form. Nothing working.

---
+++ src/components/Employees/EmployeeList.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react"
+import { useHistory } from "react-router"
+
+export const EmployeeList = () => {
+    const [employees, setEmployees] = useState([])
+    
+    const history = useHistory()
+
+    useEffect(
+        () => {
+            fetch("http://localhost:8088/employees")
+                .then(res => res.json())
+                .then((employeeArray) => {
+                    setEmployees(employeeArray)
+                })
+        },
+        []
+    )
+
+    useEffect(
+        () => {
+            const eachEmployee = employees.map(employee => employee.name)
+            setEmployees(eachEmployee.join(", "))
+        }, [employees])
+
+
+
+    return (
+        <>
+            {
+                employees.map(
+                    (employeeObj) => {
+                        return <p key={`employee--${employeeObj.id}`}>{employeeObj.name}</p>
+                    }
+                )
+            }
+            <button onClick={() => history.push("/employees/create")} className="btn btn-primary">
+                Hire Employee
+            </button>
+        </>
+    )
+}
